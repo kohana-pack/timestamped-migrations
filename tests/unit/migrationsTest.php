fb7d6670d95008f7889596876f48d93ad7b8ba46
Fedot <fedotru@gmail.com>
2013-08-06 12:57:57 +0400

    Added possibility of application of the migration for application, with the option --module=application

--- tests/unit/migrationsTest.php
+++ tests/unit/migrationsTest.php
@@ -80,6 +80,29 @@ class MigrationsTest extends Unittest_TestCase
 		return $migrations;
 	}
 
+	public function get_migration_files()
+	{
+		return array(
+			APPPATH.'migrations/123456789_test_1.php',
+			MODPATH.'test_module/migrations/123456790_test_2.php',
+			MODPATH.'test_module/migrations/123456791_test_3.php',
+		);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_kohana_modules()
+	{
+		return array(
+			'database' => MODPATH . 'database',
+			'minion' => MODPATH . 'minion',
+			'unittest' => MODPATH . 'unittest',
+			'migration' => MODPATH . 'timestamped-migrations',
+			'test_module' => MODPATH . 'test_module',
+		);
+	}
+
 	public function test_generate_new_migration_file()
 	{
 		$test_module_path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'kohana_modules_timestamped_migrations_test';
@@ -106,5 +129,50 @@ class MigrationsTest extends Unittest_TestCase
 
 		rmdir($test_module_path.DIRECTORY_SEPARATOR.'migrations');
 		rmdir($test_module_path);
+	}
+
+	public function test_get_migrations()
+	{
+		$migrations_mock = $this->getMock(
+			'Migrations',
+			array('get_migration_files', 'get_kohana_modules'),
+			array(),
+			'',
+			FALSE
+		);
+
+		$migrations_mock
+			->expects($this->any())
+			->method('get_kohana_modules')
+			->will($this->returnValue($this->get_kohana_modules()));
+
+		$migrations_mock
+			->expects($this->any())
+			->method('get_migration_files')
+			->will($this->returnValue($this->get_migration_files()));
+
+		$expected = array(
+			array(
+				'name' => '123456789_test_1',
+				'file' => APPPATH.'migrations/123456789_test_1.php',
+				'version' => '123456789',
+				'module' => 'application',
+			),
+			array(
+				'name' => '123456790_test_2',
+				'file' => MODPATH.'test_module/migrations/123456790_test_2.php',
+				'version' => '123456790',
+				'module' => 'test_module',
+			),
+			array(
+				'name' => '123456791_test_3',
+				'file' => MODPATH.'test_module/migrations/123456791_test_3.php',
+				'version' => '123456791',
+				'module' => 'test_module',
+			),
+		);
+
+		$actual = $migrations_mock->get_migrations();
+		$this->assertEquals($expected, $actual);
 	}
 }
